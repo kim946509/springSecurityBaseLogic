6ab9a6f82f74ce180db11a84a591b9d84532cae0
Your Name <kim946509@gmail.com>
2025-02-04 12:17:02 +0900

    Initial commit

---
+++ src/main/java/site/sequence/springsecuritybaselogic/SpringSecurityBaseLogicApplication.java
@@ -0,0 +1,13 @@
+package site.sequence.springsecuritybaselogic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityBaseLogicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityBaseLogicApplication.class, args);
+    }
+
+}
